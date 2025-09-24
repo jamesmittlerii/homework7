dfc46e8261e9534e5ad46c05d67a86083d98772f
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-09-23 19:53:37 -0700

    Initial Commit

---
+++ homework7/homework7App.swift
@@ -0,0 +1,17 @@
+//
+//  homework7App.swift
+//  homework7
+//
+//  Created by cisstudent on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct homework7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
